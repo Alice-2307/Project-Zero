cb476389d8da862c00969b0880f1c3c87ce7040d
Alice-2307 <adityasharma23july@gmail.com>
2023-06-13 18:52:02 +0530

    DOM

---
+++ dom.js
@@ -0,0 +1,13 @@
+// console.dir(document);
+// console.log(document.all);
+// document.all[10].textContent="hello";
+
+let headertitle = document.getElementById('header-title');
+headertitle.textContent = 'hello';
+let mainheader = document.getElementById('main-header')
+mainheader.style.borderBottom = 'solid 3px #000';
+
+let items = document.getElementsByClassName('list-group-item');
+console.log(items);
+items[0].style.fontWeight = 'bold';
+items[1].style.color = 'green';
