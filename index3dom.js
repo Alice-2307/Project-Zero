151bdbf9e330c92b0903c7ae7680b38cf5180571
Alice-2307 <adityasharma23july@gmail.com>
2023-06-14 13:43:54 +0530

    index3

---
+++ index3dom.js
@@ -0,0 +1,5 @@
+let seconditem = document.querySelector('.list-group-item:nth-child(2)');
+seconditem.style.backgroundColor = 'yellow';
+
+let thirditem =document.querySelector('.list-group-item:nth-child(3)');
+thirditem.style.visibility = 'hidden';
