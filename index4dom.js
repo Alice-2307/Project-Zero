184485fa5a8022312ef69b74e9dd904c8edf4f45
Alice-2307 <adityasharma23july@gmail.com>
2023-06-14 13:56:39 +0530

    index4

---
+++ index4dom.js
@@ -0,0 +1,6 @@
+let items = document.querySelectorAll('.list-group-item');
+items[1].style.color = 'green';
+let odd = document.querySelectorAll('li:nth-child(odd)');
+for(let i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor = 'green';
+}
