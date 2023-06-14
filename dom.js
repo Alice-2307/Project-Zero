e9a8c89b76e1b4b259526477a3a4d56e4c02ddfc
Alice-2307 <adityasharma23july@gmail.com>
2023-06-14 11:13:08 +0530

    DOM 2

--- dom.js
+++ dom.js
@@ -2,12 +2,18 @@
 // console.log(document.all);
 // document.all[10].textContent="hello";
 
-let headertitle = document.getElementById('header-title');
-headertitle.textContent = 'hello';
-let mainheader = document.getElementById('main-header')
-mainheader.style.borderBottom = 'solid 3px #000';
+// let headertitle = document.getElementById('header-title');
+// headertitle.textContent = 'hello';
+// let mainheader = document.getElementById('main-header')
+// mainheader.style.borderBottom = 'solid 3px #000';
 
-let items = document.getElementsByClassName('list-group-item');
-console.log(items);
-items[0].style.fontWeight = 'bold';
-items[1].style.color = 'green';
+// let items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// items[0].style.fontWeight = 'bold';
+// items[1].style.color = 'green';
+
+let items  = document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor = 'green';
+for(let i=0;i<items.length;i++){
+    items[i].style.fontWeight = 'bold';
+}
