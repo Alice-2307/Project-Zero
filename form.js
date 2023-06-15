88a862d3f7df1b92588c5fb163c4f480f5a032f7
Alice-2307 <adityasharma23july@gmail.com>
2023-06-15 23:44:46 +0530

    storeObject

--- form.js
+++ form.js
@@ -6,6 +6,12 @@ let email = document.querySelector('#email');
 myForm.addEventListener('submit', onSubmit);
     function onSubmit(e){
     e.preventDefault();
-    localStorage.setItem('Name', Name.value);
-    localStorage.setItem('Email', email.value);
+    // localStorage.setItem('Name', Name.value);
+    // localStorage.setItem('Email', email.value);
+    let Object_value = {
+        Name: Name.value,
+        Email: email.value
+    }
+    let convert_toString = JSON.stringify(Object_value);
+    console.log(localStorage.setItem('Object', convert_toString));
     }    
