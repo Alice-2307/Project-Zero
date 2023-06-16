199ad3e889abffe9eeb1184da6eb9e5b7f37cf37
Alice-2307 <adityasharma23july@gmail.com>
2023-06-16 19:13:13 +0530

    AddEdit

--- submitform/form.js
+++ submitform/form.js
@@ -1,55 +1,73 @@
+let myForm = document.querySelector("form");
+let Name = document.querySelector("#name");
+let email = document.querySelector("#email");
 
-let myForm = document.querySelector('form');
-let Name = document.querySelector('#name');
-let email = document.querySelector('#email');
-
-
-myForm.addEventListener('submit', onSubmit);
-    function onSubmit(e){
+myForm.addEventListener("submit", onSubmit);
+function onSubmit(e) {
     e.preventDefault();
     let key = email.value;
     let Object_value = {
         Name: Name.value,
-        Email: email.value
-    }
+        Email: email.value,
+    };
     let convert_toString = JSON.stringify(Object_value);
     console.log(localStorage.setItem(key, convert_toString));
-    }    
+}
 
-    let itemsList = document.getElementById('items');
+let itemsList = document.getElementById("items");
 
-    myForm.addEventListener('submit', addItem);
+myForm.addEventListener("submit", addItem);
 
-    function addItem(e){
-        e.preventDefault();
+function addItem(e) {
+    e.preventDefault();
 
-        let NameData = `${Name.value}- `;
-        let EmailData = email.value;
-        let newElement = document.createElement('li');
-       
-        newElement.className = 'group-item';
-        newElement.appendChild(document.createTextNode(NameData));
-        newElement.appendChild(document.createTextNode(EmailData))
-        newElement.id = EmailData;
-    
-        let deleteBtn = document.createElement('button');
-    
-        deleteBtn.className = 'delete';
-    
-        deleteBtn.appendChild(document.createTextNode('Delete'));
-    
-        newElement.appendChild(deleteBtn);
-    
-        itemsList.appendChild(newElement);
-      }
+    let NameData = `${Name.value}- `;
+    let EmailData = email.value;
 
-      itemsList.addEventListener('click', deleteItem);
+    let newElement = document.createElement("li");
+    let subElement = document.createElement("span");
 
-      function deleteItem(e){
-        if(e.target.classList.contains('delete')){
-              let dlElement = e.target.parentElement
-              localStorage.removeItem(dlElement.id);
-              console.log(dlElement.id);
-              itemsList.removeChild(dlElement);
-          }
-      }
+    newElement.className = "group-item";
+    subElement.className = "sub-group-item";
+
+    newElement.appendChild(document.createTextNode(NameData));
+    newElement.appendChild(document.createTextNode(EmailData));
+
+    newElement.id = email.value;
+    subElement.id = Name.value;
+
+    let deleteBtn = document.createElement("button");
+    let editBtn = document.createElement("button");
+
+    deleteBtn.className = "delete";
+    editBtn.className = "edit";
+
+    deleteBtn.appendChild(document.createTextNode("Delete"));
+    editBtn.appendChild(document.createTextNode("Edit"));
+
+    newElement.appendChild(deleteBtn);
+    newElement.appendChild(editBtn);
+    newElement.appendChild(subElement);
+    itemsList.appendChild(newElement);
+}
+
+itemsList.addEventListener("click", deleteItem);
+
+function deleteItem(e) {
+    if (e.target.classList.contains("delete")) {
+        let dlElement = e.target.parentElement;
+        localStorage.removeItem(dlElement.id);
+        itemsList.removeChild(dlElement);
+    }
+}
+itemsList.addEventListener("click", editItem);
+
+function editItem(e) {
+    if (e.target.classList.contains("edit")) {
+        let dlElement = e.target.parentElement;
+        localStorage.removeItem(dlElement.id);
+        Name.value = dlElement.lastChild.id;
+        email.value = dlElement.id;
+        itemsList.removeChild(dlElement);
+    }
+}
