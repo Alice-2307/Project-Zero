de0b79c6aefa79d1fb5884bab490ca8975ea85cc
Alice-2307 <adityasharma23july@gmail.com>
2023-07-04 19:01:53 +0530

    PromiseALL

--- Again/PromiseAll/promiseAll.js
+++ Again/PromiseAll/promiseAll.js
@@ -14,7 +14,7 @@ function createPost(post) {
 function updateLastUserActivityTime(){
     return new Promise((resolve,reject) => {
         setTimeout(() =>{
-           let lastActivity = new Date()
+           let lastActivity = new Date().getTime();
             resolve(lastActivity);
         }, 1000)
     })
@@ -40,14 +40,17 @@ function deletePost(){
     })
 }
 
-Promise.all([createPost({title: 'POST1', body: 'This is POST1'}),updateLastUserActivityTime()]).then((val) => {
+Promise.all([createPost({title: 'POST1', body: 'This is POST1'}),updateLastUserActivityTime()]).
+then((val) => {
     showPost();
     console.log(`Last Activity Time -> ${val[1]}`);
-    Promise.all([createPost({title: 'POST2', body: 'This is POST2'}),updateLastUserActivityTime()]).then((val1)=> {
+    Promise.all([createPost({title: 'POST2', body: 'This is POST2'}),updateLastUserActivityTime()]).
+    then((val1)=> {
         showPost();
         console.log(`Last Activity Time -> ${val1[1]}`);
         deletePost().then(() => {
-            Promise.all([createPost({title: 'POST3', body: 'This is POST3'}),updateLastUserActivityTime()]).then((val2)=> {
+            Promise.all([createPost({title: 'POST3', body: 'This is POST3'}),updateLastUserActivityTime()]).
+            then((val2)=> {
                 showPost();
                 console.log(`Last Activity Time -> ${val2[1]}`);
             })
