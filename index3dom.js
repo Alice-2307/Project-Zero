73f96bf0d52669c297fe7522dcdfe6c0e99ab5aa
Alice-2307 <adityasharma23july@gmail.com>
2023-08-04 21:23:06 +0530

    NextStep

--- index3dom.js
+++
@@ -1,5 +0,0 @@
-let seconditem = document.querySelector('.list-group-item:nth-child(2)');
-seconditem.style.backgroundColor = 'yellow';
-
-let thirditem =document.querySelector('.list-group-item:nth-child(3)');
-thirditem.style.visibility = 'hidden';
