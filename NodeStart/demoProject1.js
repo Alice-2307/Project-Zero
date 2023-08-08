42f0a34c0510f0e21b92cbb21e0d43291b5c9f28
Alice-2307 <adityasharma23july@gmail.com>
2023-08-08 06:56:36 +0530

    CleanCodeRouting

--- NodeStart/demoProject1.js
+++ NodeStart/demoProject1.js
@@ -1,37 +1,8 @@
 const val = require("http");
-const file = require("fs");
-const server = val.createServer((req, res) => {
-  const urlData = req.url;
-  const methodData = req.method;
-  const fl = file.readFileSync("dataFile.txt");
 
-  if (urlData == "/") {
-    res.write("<html>");
-    res.write("<head><title>My First Page</title></head>");
-    res.write("<body>");
-    res.write(`${fl}`);
-    res.write(
-      '<form action="/message" method="POST"><input type="text" name="anything"><button type="submit">Submit</button></form>'
-    );
-    res.write("</body>");
-    res.write("</html");
-    return res.end();
-  }
+const routes = require('./routing')
 
-  if (urlData == "/message" && methodData == "POST") {
-    const dataArr = [];
-    req.on("data", (ch) => {
-      dataArr.push(ch);
-    });
-    req.on("end", () => {
-      const parsedataArr = Buffer.concat(dataArr).toString();
-      const value = parsedataArr.split("=")[1];
-      file.writeFileSync("dataFile.txt", value);
-      res.statusCode = 302;
-      res.setHeader("Location", "/");
-      return res.end();
-    });
-  }
-});
+console.log(routes.text);
+const server = val.createServer(routes.handler);
 
 server.listen(4000);
