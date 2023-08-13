1cf766d8aff45add0eb795a0849a6a9926d784e4
Alice-2307 <adityasharma23july@gmail.com>
2023-08-13 16:20:45 +0530

    Parse

--- express.js
+++ express.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express()
 
-app.use((req,res, next) => {
-    console.log('In the MiddleWare');
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req,res, next) => {
+    res.send('<form action="/product" method="POST">Title:- <input type="text" name="title"><br><br>Size:- <input type="number" name="size"><br><br><button type="submit">Add Product</button></form>');
+    
+});
+app.use('/product', (req,res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
-app.use((req,res, next) => {
-    console.log('In another MiddleWare');
-    res.send('<h1>Hello to Node JS</h1>');
+app.use('/', (req,res, next) => {
+    res.send('<h1>Hello to Node JS World</h1>');
 });
 
 app.listen(3000)
