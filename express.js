9904adf2996602b0d66374785c27dad746f8466c
Alice-2307 <adityasharma23july@gmail.com>
2023-08-14 22:05:01 +0530

    Router&Filters

--- express.js
+++ express.js
@@ -3,19 +3,16 @@ const bodyParser = require('body-parser');
 
 const app = express()
 
+const adminRouter = require('./router/admin')
+const shopRouter = require('./router/shop')
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req,res, next) => {
-    res.send('<form action="/product" method="POST">Title:- <input type="text" name="title"><br><br>Size:- <input type="number" name="size"><br><br><button type="submit">Add Product</button></form>');
-    
-});
-app.use('/product', (req,res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
+app.use('/admin', adminRouter);
+app.use(shopRouter);
 
-app.use('/', (req,res, next) => {
-    res.send('<h1>Hello to Node JS World</h1>');
+app.use((req,res,next) => {
+    res.status(404).send('<h1>Page not found</h1>')
 });
 
 app.listen(3000)
