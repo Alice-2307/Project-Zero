19caf32de4fc9936a712bbdf5aa8ba8b0aadbfdf
Alice-2307 <adityasharma23july@gmail.com>
2023-08-20 14:59:54 +0530

    DeleteSQL

--- resources/controllers/admin.js
+++ resources/controllers/admin.js
@@ -14,8 +14,11 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null, title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  product.save()
+  .then(() => {
+    res.redirect('/');
+  })
+  .catch(err => console.log(err))
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -52,16 +55,21 @@ exports.postEditProduct = (req,res,next) => {
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.params.productId;
   console.log(prodId);
-  Product.deleteProductById(prodId);
-  res.redirect('/admin/products');
+  Product.deleteProductById(prodId)
+  .then(() => {
+    res.redirect('/admin/products');
+  })
+  .catch(err => console.log(err))
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll()
+  .then(([r,f]) => {
     res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
-    });
-  });
+          prods: r,
+          pageTitle: 'Admin Products',
+          path: '/admin/products'
+        });
+  })
+  .catch(err => console.log(err))
 };
