b3904edb56423e15253db21c90be71462e67a732
Alice-2307 <adityasharma23july@gmail.com>
2023-08-21 02:08:14 +0530

    UseBackendInBookingApp

---
+++ AppointmentBackend/router/userdata.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const userDataController = require('../controller/usercontroller');
+
+const router = express.Router();
+
+
+router.post('/addData', userDataController.postUserDetail);
+
+router.get('/getData', userDataController.getUserDetail);
+
+router.delete('/deleteData/:id', userDataController.deleteUserDetail)
+
+module.exports = router;
