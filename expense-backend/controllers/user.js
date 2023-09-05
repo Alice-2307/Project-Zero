9649875606d980dda8e9a39a7d9dacf88392778b
Alice-2307 <adityasharma23july@gmail.com>
2023-09-05 23:28:59 +0530

    addLogin

--- expense-backend/controllers/user.js
+++ expense-backend/controllers/user.js
@@ -4,7 +4,7 @@ exports.postUser = async (req, res, next) => {
     const name = req.body.name;
     const email = req.body.email;
     const pass = req.body.password;
- 
+
     try {
         let result = await user.create({
             name: name,
@@ -22,4 +22,27 @@ exports.postUser = async (req, res, next) => {
             res.status(500).json({ Error: "An error occurred" })
         }
     }
-} 
+}
+
+exports.loginUser = async (req, res, next) => {
+    const email = req.body.email;
+    const pass = req.body.password;
+
+    try {
+        let result = await user.findAll();
+
+        for (let i = 0; i < result.length; i++) {
+            if (email === result[i].email) {
+                if (pass === result[i].password) {
+                    return res.status(200).json({ success: true, message: "User logged in successfully" })
+                }
+                return res.status(404).json({ success: false, Error: "Password is incorrect" })
+            }
+        }
+        return res.status(404).json({ Error: "Email does not exist" });
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ Error: "An error occurred" })
+    }
+}
---
+++ expense-project/login.js
@@ -0,0 +1,25 @@
+const form = document.getElementById("expense");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const error = document.getElementById("error");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const loginData = {
+        email: email.value,
+        password: password.value,
+    };
+    try {
+        let result = await axios.post("http://localhost:3000/user/login", loginData);
+        console.log(result);
+        alert(`${result.data.message}`)
+
+    } catch (err) {
+        console.log(err)
+        if (err.response !== undefined) {
+            error.textContent = `Error: ${err.response.data.Error}`;
+        } else {
+            error.textContent = `Error: ${err.message}`;
+        }
+    }
+});
