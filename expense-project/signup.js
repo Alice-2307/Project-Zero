75cac55462f4f3a8c251d687cfca680c87322cf0
Alice-2307 <adityasharma23july@gmail.com>
2023-09-05 13:32:36 +0530

    signup

---
+++ expense-project/signup.js
@@ -0,0 +1,19 @@
+const form = document.getElementById("expense");
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const expenseData = {
+        name: name.value,
+        email: email.value,
+        password: password.value
+    };
+    try {
+        let data = await axios.post("http://localhost:3000/user/signup",expenseData);
+    } catch (err) {
+        const error = document.getElementById("error")
+        error.textContent = `Error: Network Error`
+    }
+});
